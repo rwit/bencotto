94e065b10472e60edfec3cbb78114406b6c7389d
Ruud Withagen <ruud.withagen@gmail.com>
2012-02-26 14:35:37 +0100

    fixed login

--- bencotto/urls.py
+++ bencotto/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth.views import login, logout
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', login),
+    url(r'^$', login, {'template_name': 'login.html'}),
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^accounts/logout/$', logout),
-    #url(r'^accounts/profile/$', 'accounts.views.profile')
+    url(r'^accounts/logout/$', logout),
+    url(r'^accounts/profile/$', 'rusk.views.profile')
 )
 
