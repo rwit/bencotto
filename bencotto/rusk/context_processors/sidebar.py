abe6ca941f1ca7136e0cbd26bbf09522d4aca6d6
Paul Bormans <peebor@gmail.com>
2012-03-05 21:52:47 +0100

    Added the query for most likes rusk (aggregation)

--- bencotto/rusk/context_processors/sidebar.py
+++ bencotto/rusk/context_processors/sidebar.py
@@ -1,11 +1,12 @@
-from bencotto.rusk.models import rusk
+from django.db.models import Count
+from bencotto.rusk.models import rusk, likes
 
 def sidebarLikedMostRusk(request):
-    #@todo: query usng joins
-    r = rusk.objects.all().order_by('-date_added')[:1]
-    if(r.count() == 1):
+    #select the rusk that is likes the most using aggregation
+    l = likes.objects.annotate(num_likes=Count('id')).order_by('-num_likes')[:1]
+    if(l.count() == 1):
         return dict({
-            'likedMostRusk': r,
+            'likedMostRusk': l[0].rusk,
         })
     else:
         #Empty dict to indicate no newest rusk available
@@ -15,7 +16,7 @@ def sidebarMostViewedRusk(request):
     r = rusk.objects.all().order_by('-views')[:1]
     if(r.count() == 1):
         return dict({
-            'mostViewedRusk': r,
+            'mostViewedRusk': r[0],
         })
     else:
         #Empty dict to indicate no newest rusk available
@@ -25,7 +26,7 @@ def sidebarNewestRusk(request):
     r = rusk.objects.all().order_by('-date_added')[:1]
     if(r.count() == 1):
         return dict({
-            'newestRusk': r,
+            'newestRusk': r[0],
         })
     else:
         #Empty dict to indicate no newest rusk available
