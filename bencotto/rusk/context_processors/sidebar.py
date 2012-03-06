40d0b47a6b37111b0f211b15c3da8e0e7998584c
Paul Bormans <peebor@gmail.com>
2012-03-06 10:20:08 +0100

    Added the query to select the rusk most liked; nice example of annotate

--- bencotto/rusk/context_processors/sidebar.py
+++ bencotto/rusk/context_processors/sidebar.py
@@ -2,33 +2,39 @@ from django.db.models import Count
 from bencotto.rusk.models import rusk, likes
 
 def sidebarLikedMostRusk(request):
-    #select the rusk that is likes the most using aggregation
-    l = likes.objects.annotate(num_likes=Count('id')).order_by('-num_likes')[:1]
-    if(l.count() == 1):
+    #select the rusk that is liked the most using aggregation
+    l = likes.objects.annotate(num_likes=Count('id')).order_by('-num_likes').values('rusk__title', 'rusk__description', 'num_likes')[:1]
+    #SELECT "rusk_rusk"."title", "rusk_rusk"."description", COUNT("rusk_likes"."id") AS "num_likes" 
+    #FROM "rusk_likes" 
+    #INNER JOIN "rusk_rusk" ON ("rusk_likes"."rusk_id" = "rusk_rusk"."id") 
+    #GROUP BY "rusk_likes"."id", "rusk_likes"."user_id", "rusk_likes"."rusk_id", "rusk_rusk"."title", "rusk_rusk"."description" 
+    #ORDER BY "num_likes" 
+    #DESC LIMIT 1    
+    try:
         return dict({
-            'likedMostRusk': l[0].rusk,
+            'likedMostRusk': l[0], #Only issue here is that the returned object is in fact not a rusk but contains referenced items like rusk__title
         })
-    else:
+    except IndexError:
         #Empty dict to indicate no newest rusk available
         return dict()
 
 def sidebarMostViewedRusk(request):
     r = rusk.objects.all().order_by('-views')[:1]
-    if(r.count() == 1):
+    try:
         return dict({
             'mostViewedRusk': r[0],
         })
-    else:
+    except IndexError:
         #Empty dict to indicate no newest rusk available
         return dict()
 
 def sidebarNewestRusk(request):
     r = rusk.objects.all().order_by('-date_added')[:1]
-    if(r.count() == 1):
+    try:
         return dict({
             'newestRusk': r[0],
         })
-    else:
+    except IndexError:
         #Empty dict to indicate no newest rusk available
         return dict()
 
