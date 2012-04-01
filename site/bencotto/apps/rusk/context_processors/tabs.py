ec001a785e8412e9c16d0ef3f27459ef630eb785
Paul Bormans <peebor@gmail.com>
2012-04-01 22:08:20 +0200

    Added try/except to ignore reverse exceptions in unittests outside rusk

--- site/bencotto/apps/rusk/context_processors/tabs.py
+++ site/bencotto/apps/rusk/context_processors/tabs.py
@@ -1,5 +1,5 @@
 import urllib
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, NoReverseMatch
 
 def isActive(path, pattern):
     import re
@@ -8,14 +8,18 @@ def isActive(path, pattern):
 def tabs(request):
     """generate the parameters required to build the tabs"""
     tabs = []
-    tabs.append(dict({ 'title':'Popular rusks', 'link':reverse('popular'), 'is_active':isActive(request.path, 'popular')}))
-    tabs.append(dict({ 'title':'Latest rusks', 'link':reverse('latest'), 'is_active':isActive(request.path, 'latest')}))
-    if request.user.is_authenticated():
-        tabs.append(dict({ 'title':'Add a rusk', 'link':reverse('add_rusk'), 'is_active':isActive(request.path, 'add')}))
-        tabs.append(dict({ 'title':'My rusks', 'link':reverse('rusks'), 'is_active':isActive(request.path, 'rusks')}))
-        tabs.append(dict({ 'title':'My friends rusks', 'link':reverse('friends'), 'is_active':isActive(request.path, 'friends')}))
-        tabs.append(dict({ 'title':'Notifications', 'link':reverse('notifications'), 'is_active':isActive(request.path, 'notifications')}))
-        tabs.append(dict({ 'title':'Logout', 'link':reverse('logout'), 'is_active':isActive(request.path, 'logout')}))
-    else:
-        tabs.append(dict({ 'title':'Login', 'link':reverse('login') + '?' + urllib.urlencode({'next':'popular'}), 'is_active':isActive(request.path, 'login')}))
+    try:
+        tabs.append(dict({ 'title':'Popular rusks', 'link':reverse('popular'), 'is_active':isActive(request.path, 'popular')}))
+        tabs.append(dict({ 'title':'Latest rusks', 'link':reverse('latest'), 'is_active':isActive(request.path, 'latest')}))
+        if request.user.is_authenticated():
+            tabs.append(dict({ 'title':'Add a rusk', 'link':reverse('add_rusk'), 'is_active':isActive(request.path, 'add')}))
+            tabs.append(dict({ 'title':'My rusks', 'link':reverse('rusks'), 'is_active':isActive(request.path, 'rusks')}))
+            tabs.append(dict({ 'title':'My friends rusks', 'link':reverse('friends'), 'is_active':isActive(request.path, 'friends')}))
+            tabs.append(dict({ 'title':'Notifications', 'link':reverse('notifications'), 'is_active':isActive(request.path, 'notifications')}))
+            tabs.append(dict({ 'title':'Logout', 'link':reverse('logout'), 'is_active':isActive(request.path, 'logout')}))
+        else:
+            tabs.append(dict({ 'title':'Login', 'link':reverse('login') + '?' + urllib.urlencode({'next':'popular'}), 'is_active':isActive(request.path, 'login')}))
+    except NoReverseMatch, e:
+        #Ignore exception here because tests outside Rusk do parse the global processors but don't include the Rusk urlconf
+        pass 
     return dict({'tabs':tabs})
